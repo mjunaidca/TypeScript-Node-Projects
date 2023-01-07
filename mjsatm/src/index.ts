a3be570154f065e8d57498af9c2087f730a92f37
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-01-07 17:45:38 +0500

    Published Project on NPM

--- mjsatm/src/index.ts
+++ mjsatm/src/index.ts
@@ -1,4 +1,13 @@
+#! /usr/bin/env node
+
 import users from "./users.js";
 import { login } from "./login.js";
+
+
 console.log(`userId: ${users[0].userId} \nPin: ${users[0].pinCode}\n`);
-login();
+
+login()
+
+
+
+
