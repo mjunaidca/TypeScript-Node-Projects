da761e831a3dd71387cd7e1529223e1b0342eef9
Muhammad Junaid <mr.junaidshaukat@gmail.com>
2023-01-09 14:20:00 +0500

    Published Student Management System on NPM

--- mjs-sms/src/index.ts
+++ mjs-sms/src/index.ts
@@ -1,4 +1,16 @@
+#! /usr/bin/env node
+
 import { get_started, welcome } from "./MainScreen.js";
+import { studentsData, instructorsData } from "./DataBase.js";
+import chalk from "chalk";
+
+let ins = `Email: ${instructorsData[0].instructorEmail}\nPassword: ${instructorsData[0].instructorPassword}`;
+let stu = `Email: ${studentsData[0].studentEmail}\nPassword: ${studentsData[0].studentPassword}`;
+
+
+console.log(chalk.white(`\nInstructor Login: \n${ins}\n`));
+
+console.log(chalk.white(`Student Login: \n${stu}\n`));
 
 welcome()
 
